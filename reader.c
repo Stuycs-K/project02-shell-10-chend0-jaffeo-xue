e54badb544a4a013cf887ecad2242dc5eb2405fd
David Chen <dchen07@protonmail.com>
2024-12-02 20:00:29 -0500

    Improve reader documentation

--- reader.c
+++ reader.c
@@ -1,5 +1,5 @@
 /*
- * Reads a newline-delimited line of input from stdin.
+ * Reads a newline-delimited line of input from stdin. May exit on EOF if no buffered input is found.
  * WARNING: malloc()s a new block of data potentially larger than user input. Do not forget to free().
  * Returns the read line of input.
  */
