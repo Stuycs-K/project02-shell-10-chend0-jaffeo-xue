bf823e1b6b0b9f075af1ff9f5ae483ee5b98686c
yellowstoneparks <otzarj@nycstudents.net>
2024-12-03 12:09:18 -0500

    exit code

--- main.c
+++ main.c
@@ -2,6 +2,7 @@
 #include "parser.h"
 #include "proompt.h"
 #include "reader.h"
+#include <string.h>
 
 int main(void) {
 	while (1) {
@@ -9,7 +10,9 @@ int main(void) {
 		char * line = readLine();
 		unsigned long n;
 		char * cmds[] = parseCmds(&n);  // handle semicolons, length in n
-		for (unsigned long i = 0; i < n; i++) {
+		char * exit[] = "exit";
+		if (strcmp(cmds[0], exit) == 0) {exit();}
+		for (unsigned long i = 0; i < n; i++) { 
 			execute(cmds[i]);
 		}
 		// free(line);  // maybe we need this later
