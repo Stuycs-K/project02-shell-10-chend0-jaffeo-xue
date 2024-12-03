e100eadfd596b1f94c34797787ed688fa452d532
David Chen <dchen07@protonmail.com>
2024-12-02 22:50:07 -0500

    Fix macro usage

--- proompt.c
+++ proompt.c
@@ -11,47 +11,21 @@
  * Not available for use outside of `proompt.c`.
  */
 
-#ifdef _GNU_SOURCE
 char *getPath() {
-    return "";
-    // uses glibc extension to getcwd(), technically not portable but should be
-    // fine
+#ifdef __GLIBC__
+    // uses glibc extension to getcwd()
     char *cwd = getcwd(NULL, 0);
-    char *_home;
-    // we need $HOME to know what `~` should stand for, no other case is allowed
-    if (_home = getenv("HOME")) {
-        // getenv()'s returned string could be statically allocated, to avoid
-        // race conditions we copy
-        unsigned int n = strlen(_home);
-        char home[n + 1];
-        strncpy(home, _home, n);
-        home[n] = '\0'; // taking no chances
-        for (unsigned int i = 0; i <= n; i++) {
-            if (home[i] == '\0') {
-                // vibe check passed
-                // note that only weird systems have trailing / on $HOME they
-                // can cope: comment under https://serverfault.com/a/876318
-                unsigned int m = strlen(cwd);
-                cwd[0] = '~';
-                for (unsigned int j = 1; i <= m; i++, j++) {
-                    cwd[j] = cwd[i];
-                }
-            }
-            if (home[i] != cwd[i])
-                break;
-        }
-    }
-    return cwd;
-}
+    if (!cwd) { // fallthrough
 #else
-char *getPath() {
-    // uses glibc extension to getcwd(), technically not portable but should be
-    // fine
+    // for the musl users maybe
+    char *cwd = malloc(1024);
+    if (!getcwd(cwd, 1024)) {
+#endif
+        perror("getcwd");
+        cwd[0] = '\0';
+        return cwd;
+    }
 
-    char buffer[5000];
-    char *cwd = getcwd(buffer, sizeof(buffer));
-
-    // char *cwd = malloc(sizeof(char) * 200);
     char *_home;
     // we need $HOME to know what `~` should stand for, no other case is allowed
     if ((_home = getenv("HOME"))) {
@@ -76,11 +50,8 @@ char *getPath() {
                 break;
         }
     }
-    char *new_c = malloc(sizeof(char) * 500);
-    strcpy(new_c, cwd);
-    return new_c;
+    return cwd;
 }
-#endif
 
 /*
  * Reads the CWD and UID and prints an appropriate Bash-style prompt.
@@ -88,7 +59,7 @@ char *getPath() {
 void printPrompt() {
     char suffix = geteuid() ? '$' : '#'; // eUID == 0 for root
     char *direc = getPath();
-    printf("[ %s ]%c \n", direc, suffix);
+    printf("[ %s ]%c ", direc, suffix);
     free(direc);
     fflush(stdout);
 }
