b01b8f2d4c33c490c479f9cb3ad1b24a699cc559
Elias Xu <124101026+Elias2660@users.noreply.github.com>
2024-12-02 22:25:51 -0500

    screaming and crying in c indentation

--- main.c
+++ main.c
@@ -4,15 +4,15 @@
 #include "reader.h"
 
 int main(void) {
-	while (1) {
-		printPrompt();
-		char * line = readLine();
-		unsigned long n;
-		char ** cmds = parseCmds(line, &n);  // handle semicolons, length in n
-		for (unsigned long i = 0; i < n; i++) {
-			execute(cmds[i]);
-		}
-		// free(line);  // maybe we need this later
-	}
-	return 0;
+    while (1) {
+        printPrompt();
+        char *line = readLine();
+        unsigned long n = 0;
+        char **cmds = parseCmds(line, &n); // handle semicolons, length in n
+        for (unsigned long i = 0; i < n; i++) {
+            execute(cmds[i]);
+        }
+        // free(line);  // maybe we need this later
+    }
+    return 0;
 }
--- proompt.c
+++ proompt.c
@@ -4,48 +4,91 @@
 #include <unistd.h>
 
 /*
- * Reads the CWD, replacing leading instances of the environment variable $HOME with ~.
- * WARNING: Implicitly malloc()s a buffer for the path, return value should be free()d.
- * Trailing bytes may be allocated, and should be ignored.
+ * Reads the CWD, replacing leading instances of the environment variable $HOME
+ * with ~. WARNING: Implicitly malloc()s a buffer for the path, return value
+ * should be free()d. Trailing bytes may be allocated, and should be ignored.
  * Returns the string after ~ replacement.
  * Not available for use outside of `proompt.c`.
  */
-char * getPath() {
-	// uses glibc extension to getcwd(), technically not portable but should be fine
-	char* cwd = getcwd(NULL, 0);
-	char* _home;
-	// we need $HOME to know what `~` should stand for, no other case is allowed
-	if (_home = getenv("HOME")) {
-		// getenv()'s returned string could be statically allocated, to avoid race conditions we copy
-		unsigned int n = strlen(_home);
-		char home[n+1];
-		strncpy(home, _home, n);
-		home[n] = '\0';  // taking no chances
-		for (unsigned int i = 0; i <= n; i++) {
-			if (home[i] == '\0') {
-				// vibe check passed
-				// note that only weird systems have trailing / on $HOME they can cope: comment under https://serverfault.com/a/876318
-				unsigned int m = strlen(cwd);
-				cwd[0] = '~';
-				for (unsigned int j = 1; i <= m; i++,j++) {
-					cwd[j] = cwd[i];
-				}
-			}
-			if (home[i] != cwd[i])
-				break;
-		}
-	}
-	return cwd;
-}
 
+#ifdef _GNU_SOURCE
+char *getPath() {
+    return "";
+    // uses glibc extension to getcwd(), technically not portable but should be
+    // fine
+    char *cwd = getcwd(NULL, 0);
+    char *_home;
+    // we need $HOME to know what `~` should stand for, no other case is allowed
+    if (_home = getenv("HOME")) {
+        // getenv()'s returned string could be statically allocated, to avoid
+        // race conditions we copy
+        unsigned int n = strlen(_home);
+        char home[n + 1];
+        strncpy(home, _home, n);
+        home[n] = '\0'; // taking no chances
+        for (unsigned int i = 0; i <= n; i++) {
+            if (home[i] == '\0') {
+                // vibe check passed
+                // note that only weird systems have trailing / on $HOME they
+                // can cope: comment under https://serverfault.com/a/876318
+                unsigned int m = strlen(cwd);
+                cwd[0] = '~';
+                for (unsigned int j = 1; i <= m; i++, j++) {
+                    cwd[j] = cwd[i];
+                }
+            }
+            if (home[i] != cwd[i])
+                break;
+        }
+    }
+    return cwd;
+}
+#else
+char *getPath() {
+    // uses glibc extension to getcwd(), technically not portable but should be
+    // fine
+
+    char buffer[5000];
+    char *cwd = getcwd(buffer, sizeof(buffer));
+
+    // char *cwd = malloc(sizeof(char) * 200);
+    char *_home;
+    // we need $HOME to know what `~` should stand for, no other case is allowed
+    if ((_home = getenv("HOME"))) {
+        // getenv()'s returned string could be statically allocated, to avoid
+        // race conditions we copy
+        unsigned int n = strlen(_home);
+        char home[n + 1];
+        strncpy(home, _home, n);
+        home[n] = '\0'; // taking no chances
+        for (unsigned int i = 0; i <= n; i++) {
+            if (home[i] == '\0') {
+                // vibe check passed
+                // note that only weird systems have trailing / on $HOME they
+                // can cope: comment under https://serverfault.com/a/876318
+                unsigned int m = strlen(cwd);
+                cwd[0] = '~';
+                for (unsigned int j = 1; i <= m; i++, j++) {
+                    cwd[j] = cwd[i];
+                }
+            }
+            if (home[i] != cwd[i])
+                break;
+        }
+    }
+    char *new_c = malloc(sizeof(char) * 500);
+    strcpy(new_c, cwd);
+    return new_c;
+}
+#endif
 
 /*
  * Reads the CWD and UID and prints an appropriate Bash-style prompt.
  */
 void printPrompt() {
-	char suffix = geteuid() ? '$' : '#';  // eUID == 0 for root
-	char *direc = getPath();
-	printf("[ %s ]%c ", direc, suffix);
-	free(direc);
-	fflush(stdout);
+    char suffix = geteuid() ? '$' : '#'; // eUID == 0 for root
+    char *direc = getPath();
+    printf("[ %s ]%c \n", direc, suffix);
+    free(direc);
+    fflush(stdout);
 }
