47e6588bb0693dcd6df5b82a9bfc6ee1ba5e8f7f
Elias Xu <124101026+Elias2660@users.noreply.github.com>
2024-12-03 09:23:28 -0500

    add parse args function

--- executor.c
+++ executor.c
@@ -1,5 +1,10 @@
-#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
+#include <unistd.h>
+
+#define NUMBER_OF_ARGS_MAX 30
+#define MAX_SIZE_ARG 20
 
 /*
  * Turns a space-separated command line into an array of words.
@@ -7,13 +12,25 @@
  * Returns the NULL-terminated array of words.
  * Not available for use outside of `executor.c`.
  */
-char** parseCmd(char * command) {
-	return 0;
+char **parseCmd(char *c) {
+    char *arg_ary[NUMBER_OF_ARGS_MAX];
+    int arg_index = 0;
+    char *command = strdup(c);
+    while (strstr(command, " ") != NULL) {
+        arg_ary[arg_index] = strsep(&command, " ");
+        arg_index++;
+    }
+    arg_ary[arg_index] = command;
+    arg_ary[arg_index + 1] = NULL;
+    char **memorized_elements = malloc((arg_index + 1) * sizeof(char *));
+    for (int i = 0; i < (arg_index + 1); i++) {
+        memorized_elements[i] = malloc(sizeof(char) * MAX_SIZE_ARG);
+        strcpy(memorized_elements[i], arg_ary[i]);
+    }
+    return memorized_elements;
 }
 
 /*
  * Parses and executes the command given in `command` using execvp().
  */
-void execute(char *command) {
-
-}
+void execute(char *command) {}
