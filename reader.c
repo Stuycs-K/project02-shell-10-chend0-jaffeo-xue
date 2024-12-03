8d9030192596fc0b451b64515e3193c3e51cf105
David Chen <dchen07@protonmail.com>
2024-12-03 14:03:25 -0600

    Temporary line reader

--- reader.c
+++ reader.c
@@ -1,8 +1,14 @@
+#include <stdio.h>
 /*
  * Reads a newline-delimited line of input from stdin. May exit on EOF if no buffered input is found.
  * WARNING: malloc()s a new block of data potentially larger than user input. Do not forget to free().
  * Returns the read line of input.
  */
+
+// kludge for now
+#define SIZE_STATIC 256
 char * readLine() {
-	return "";
+    char * ret = malloc(SIZE_STATIC * (sizeof(char)));
+    fgets(ret, SIZE_STATIC, stdin);
+    return ret;
 }
