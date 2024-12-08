084c9ab6c4963d8c5cd855b97bb913c9da642dd0
David Chen <dchen07@protonmail.com>
2024-12-08 14:06:08 -0500

    Short-circuit failed pipelines

--- executor.c
+++ executor.c
@@ -32,8 +32,8 @@ void print_char_ss(char **args) {
  * @param arg_ary: The array of strings to slice.
  * @param start: The starting index of the slice.
  * @param end: The ending index of the slice.
- * @param extra: The number of extra elements to allocate (to prevent a malloc
- * error).
+ * @param extra: The number of extra spaces to allocate (to prevent buffer
+ * overruns).
  * @return: The sliced array of strings.
  */
 char **slice(char **arg_ary, int start, int end, int extra) {
@@ -80,15 +80,15 @@ void parse_args(char *command, char **arg_ary) {
  * @param output: The output mode (0 for no output, 1 for output, 2 for append).
  * @param output_file: The file to write the output to.
  * @param input_file: The file to read the input from.
- * @return: void
+ * @return: 0 if execution succeeded, errno if failed; exits program on `exit`
  */
-void run(char **args, char input, char output, char *input_file,
-         char *output_file) {
+int run(char **args, char input, char output, char *input_file,
+        char *output_file) {
     int orig_stdout = STDOUT_FILENO, orig_stdin = STDIN_FILENO;
     int backup_stdout = dup(orig_stdout), backup_stdin = dup(orig_stdin);
 
     if (args[0] == NULL || args[0][0] == '\0')
-        return;
+        return 0;
     else if (!strcmp(args[0], "cd")) {
         char *path = args[1]; // in worst case is NULL
         if (!path)
@@ -102,14 +102,14 @@ void run(char **args, char input, char output, char *input_file,
         p = fork();
         if (p < 0) {
             perror("fork");
-            return;
+            return errno;
         } else if (p == 0) {
             // input
             if (input > 0) {
                 int f_in = open(input_file, O_RDONLY, 0644);
                 if (f_in == -1) {
                     perror(input_file);
-                    return;
+                    return errno;
                 }
                 dup2(f_in, orig_stdin);
                 close(f_in);
@@ -128,14 +128,14 @@ void run(char **args, char input, char output, char *input_file,
                     perror("run(): output");
                     dup2(backup_stdin, orig_stdin);
                     close(backup_stdin);
-                    return;
+                    return errno;
                 }
                 fd1 = open(output_file, flags, 0644);
                 if (fd1 == -1) {
                     perror(output_file);
                     dup2(backup_stdin, orig_stdin);
                     close(backup_stdin);
-                    return;
+                    return errno;
                 }
                 dup2(fd1, orig_stdout);
                 close(fd1);
@@ -151,15 +151,16 @@ void run(char **args, char input, char output, char *input_file,
     dup2(backup_stdin, orig_stdin);
     close(backup_stdin);
     close(backup_stdout);
+    return 0;
 }
 
 /*
  * Executes the given command.
  * Not available for use outside of `executor.c`.
- * @param args: The command to execute.
- * @return: void
+ * @param args: The command to execute, after parsing by parse_args().
+ * @return: 0 if successful, `errno` from run() if failed
  */
-void execute_commands(char **args) {
+int execute_commands(char **args) {
     char **new_args = NULL;
     char *input_file = NULL, *output_file = NULL;
 
@@ -169,7 +170,7 @@ void execute_commands(char **args) {
 
     for (int i = 0; args[i] != NULL; i++) {
         if (strcmp(args[i], "<") == 0 || strcmp(args[i], ">") == 0 ||
-            strcmp(args[i], ">>") == 0 || args[i + 1] == NULL) {
+            strcmp(args[i], ">>") == 0) {
             if (strcmp(args[i], "<") == 0) {
                 i += 1;
                 input_file = args[i];
@@ -184,38 +185,38 @@ void execute_commands(char **args) {
                 if (new_args == NULL) {
                     new_args = slice(args, index_last_command, i - 1, 0);
                 }
-            } else if (strcmp(args[i], ">>") == 0) {
+            } else /* if (strcmp(args[i], ">>") == 0) */ {
                 i += 1;
                 output_file = args[i];
                 output_mode = 2;
                 if (new_args == NULL) {
                     new_args = slice(args, index_last_command, i - 1, 0);
                 }
-            } else if (args[i + 1] == NULL) {
-                break;
             }
             index_last_command = i + 1;
         }
     }
 
     if (new_args != NULL) {
-        run(new_args, input_mode, output_mode, input_file, output_file);
+        int ret =
+            run(new_args, input_mode, output_mode, input_file, output_file);
         free(new_args);
+        return ret;
     } else {
-        run(args, input_mode, output_mode, input_file, output_file);
+        return run(args, input_mode, output_mode, input_file, output_file);
     }
 }
 
 /*
- * Reorganizes the command to handle pipes and executes it.
+ * Reorganizes the command to fake a single pipe and executes it.
  * Not available for use outside of `executor.c`.
- * @param args: The command to reorganize.
+ * @param args: The command to process and execute.
  * @return: void
  */
 void reorganize_pipe(char **args) {
     int pipe = -1, size = 0;
     for (int i = 0; args[i] != NULL; i++) {
-        if (strcmp(args[i], "|") == 0) {
+        if (pipe < 0 && strcmp(args[i], "|") == 0) {
             pipe = i;
         }
         size = i + 1;
@@ -226,14 +227,18 @@ void reorganize_pipe(char **args) {
         char *temporary_file = NULL;
 
         for (int i = 0; i < pipe - 1; i++) {
-            if (strstr(args[i], ">") != NULL || strstr(args[i], ">>") != NULL) {
-                temporary_file =
-                    malloc(sizeof(char) * (strlen(args[i + 1]) + 1));
+            if (strstr(args[i], ">") !=
+                NULL /* || strstr(args[i], ">>") != NULL */) {
+                if (temporary_file)
+                    free(temporary_file);
+                size_t len = strlen(args[i + 1]) + 1;
+                temporary_file = malloc(sizeof(char) * len);
                 if (!temporary_file) {
-                    perror("malloc for temporary file: ");
+                    perror("reorganize_pipe: malloc");
                     exit(errno);
                 }
-                strcpy(temporary_file, args[i + 1]);
+                strncpy(temporary_file, args[i + 1], len);
+                temporary_file[len - 1] = '\0'; // safety first!
             }
         }
         if (temporary_file == NULL) {
@@ -250,15 +255,18 @@ void reorganize_pipe(char **args) {
         args2[len_args2] = "<";
         args2[len_args2 + 1] = temporary_file;
         args2[len_args2 + 2] = NULL;
-        execute_commands(args1);
-        execute_commands(args2);
+        if (!execute_commands(args1))
+            execute_commands(args2);
 
         if (strcmp(temporary_file, "temp.txt") == 0) {
-            if (remove(temporary_file) == -1)
-                perror("pipe buffer");
+            if (remove(temporary_file) == -1 && errno != ENOENT)
+                perror("reorganize_pipe(): temp.txt");
         }
         free(args1);
         free(args2);
+        // [unfree()able] string constant will have same address everywhere
+        if (temporary_file != "temp.txt")
+            free(temporary_file);
     } else {
         execute_commands(args);
     }
